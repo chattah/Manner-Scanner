f3e6bbb090596cfa64f8da8809159f80039afba4
Joel <chattah@gmail.com>
2016-12-20 23:42:32 -0600

    Setup the basic scanning functionality.  Runs pretty slow right now

--- Manner Scanner/MainWindow.xaml.cs
+++ Manner Scanner/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Manner_Scanner
 {
@@ -23,6 +26,54 @@ namespace Manner_Scanner
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void button_Click(object sender, RoutedEventArgs e)
+		{
+			int startPort = Convert.ToInt16(portsFrom.Text);
+			int endPort = Convert.ToInt16(portsTo.Text);
+
+			scanProgress.Value = 0;
+			scanProgress.Maximum = endPort - startPort + 1;
+
+			output.Text = "";
+			DoEvents();
+
+			for (int curr = startPort; curr <= endPort; curr++)
+			{
+				TcpClient scan = new TcpClient();
+
+
+				try
+				{
+					scan.Connect(ipAddress.Text, curr);
+					output.AppendText("Port " + curr + " open.\n");
+					DoEvents();
+				}
+				catch (Exception)
+				{
+					output.AppendText("Port " + curr + " closed.\n");
+					DoEvents();
+				}
+
+				scanProgress.Value = scanProgress.Value + 1;
+			}
+		}
+
+		public void DoEvents()
+		{
+			DispatcherFrame frame = new DispatcherFrame(true);
+			Dispatcher.CurrentDispatcher.BeginInvoke
+			(
+			DispatcherPriority.Background,
+			(SendOrPostCallback)delegate (object arg)
+			{
+				var f = arg as DispatcherFrame;
+				f.Continue = false;
+			},
+			frame
+			);
+			Dispatcher.PushFrame(frame);
 		}
 	}
 }
